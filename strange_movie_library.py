eb9f8969477de6ecc033e4f5a1e1933cabfbbb69
Shawn Mitchell <shawnmitch12@gmail.com>
2023-09-13 20:25:44 -0700

    Imported the data, classes, and intro into the main script

--- strange_movie_library.py
+++ strange_movie_library.py
@@ -1,4 +1,11 @@
+#import the movie data, the classes, and the intro into the main script
 from classes import *
 from library_data import *
+from intro import *
+
+#play the intro
+print_intro()
+
+
 
 
