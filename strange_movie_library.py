f74fbd1dd458eff187efd712544d1fac88810d8b
Shawn Mitchell <shawnmitch12@gmail.com>
2023-09-13 20:44:00 -0700

    Finished writing 2 functions to create the linked list in the main script

--- strange_movie_library.py
+++ strange_movie_library.py
@@ -7,5 +7,29 @@ from intro import *
 print_intro()
 
 
+#Creating a function to insert movie genres into the LinkedList
+def insert_genre():
+    movie_genre_list = LinkedList()
+    for movie_genre in genres:
+        movie_genre_list.insert_start(movie_genre)
+    return movie_genre_list
 
+#Creating function to insert movies into a nested LinkedList of the genre LinkedList
+def insert_movie_list():
+    movie_data_list = LinkedList()
+    for movie_genre in genres:
+        movie_sublist = LinkedList()
+        for movie in movie_list:
+            if movie[0] == movie_genre:
+                movie_sublist.insert_start(movie)
+        movie_data_list.insert_start(movie_sublist)
+    return movie_data_list
+
+#Create lists for user choices
+my_genre_list = insert_genre()
+my_movie_list = insert_movie_list()
+
+selected_movie_genre = ""
+
+#Create code for User Interaction
 
